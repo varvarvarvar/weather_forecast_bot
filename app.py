cdf35509144e728907ecb305a35c7a616f4ab4f6
varvara-krasavina <varvara.krasavina@gmail.com>
2020-08-12 20:04:11 +0300

    Accept geolocation as input instead of lat and long

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 
+from geopy.geocoders import Nominatim
+
 from config import YA_TOKEN
 from src import YaWeather, YaWeatherParser
 
@@ -8,12 +10,17 @@ app = Flask(__name__)
 ya_api = YaWeather(YA_TOKEN)
 parser = YaWeatherParser()
 
+geolocator = Nominatim(user_agent='myapplication')
+
 
 @app.route('/weather/api/v1.0/', methods=['POST'])
 def forecast():
 
     response = request.json
-    lat, lon = float(response['lat']), float(response['lon'])
+    location = response['location']
+
+    location = geolocator.geocode(location)
+    lat, lon = location.raw['lat'], location.raw['lon']
 
     weather_data = ya_api.get_weather(lat=lat, lon=lon)
     weather_desc = parser.parse(weather_data)
@@ -22,4 +29,4 @@ def forecast():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    app.run(debug=True, port=5000)
