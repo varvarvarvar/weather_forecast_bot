a8903be1e651a125827d525cc5f406b90fc5a12b
varvara-krasavina <varvara.krasavina@gmail.com>
2020-08-13 14:34:51 +0300

    Refactoring; error handling if weather_data is None

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
 from flask import Flask, jsonify, request
 
-from geopy.geocoders import Nominatim
-
 from config import YA_TOKEN
-from src import YaWeather, YaWeatherDescriptor
+from src import YaWeatherDescriptor
 
 app = Flask(__name__)
 
-ya_api = YaWeather(YA_TOKEN)
-descriptor = YaWeatherDescriptor()
-
-geolocator = Nominatim(user_agent='myapplication')
+descriptor = YaWeatherDescriptor(YA_TOKEN)
 
 
 @app.route('/weather/api/v1.0/', methods=['GET'])
 def forecast():
 
     response = request.json
-    input_location = response['location']
+    location = response['location']
 
-    location = geolocator.geocode(input_location)
-    if not location:
-        weather_desc = "I'm sorry but I couldn't parse this location."
-    else:
-        lat, lon = location.raw['lat'], location.raw['lon']
-
-        weather_data = ya_api.get_weather(lat=lat, lon=lon)
-        weather_desc = descriptor.describe(weather_data)
+    weather_desc = descriptor.describe(location)
 
     return jsonify(
-        {'description': weather_desc, 'location': input_location}
+        {'description': weather_desc, 'location': location}
     ), 201
 
 
--- src.py
+++ src.py
@@ -1,6 +1,8 @@
 import logging
 import requests
 
+from geopy.geocoders import Nominatim
+
 
 class YaWeather:
 
@@ -34,12 +36,43 @@ class YaWeather:
 
 
 class YaWeatherDescriptor:
-    def __init__(self):
-        pass
+    def __init__(self, ya_api_token):
 
-    def describe(self, weather_data):
+        self.ya_api = YaWeather(ya_api_token)
+        self.geolocator = Nominatim(user_agent='myapplication')
+
+    def form_description(self, weather_data):
+
+        if 'fact' not in weather_data \
+                or 'temp' not in weather_data['fact'] \
+                or 'feels_like' not in weather_data['fact'] \
+                or 'condition' not in weather_data['fact']:
+            return None
+
         weather_desc = 'Temperature: %sC, feels like: %sC, %s.' % (
-            weather_data['fact']['temp'], weather_data['fact']['feels_like'],
+            weather_data['fact']['temp'],
+            weather_data['fact']['feels_like'],
             weather_data['fact']['condition']
         )
+        return weather_desc
+
+    def describe(self, input_location):
+
+        location = self.geolocator.geocode(input_location)
+
+        if not location:
+            return "Sorry, but I couldn't parse this location."
+
+        lat, lon = location.raw['lat'], location.raw['lon']
+
+        weather_data = self.ya_api.get_weather(lat=lat, lon=lon)
+
+        if not weather_data:
+            return 'Sorry, an internal error occurred.'
+
+        weather_desc = self.form_description(weather_data)
+
+        if not weather_desc:
+            return 'Sorry, an internal error occurred.'
+
         return weather_desc
